1584929ebbaf77653ac274bc5e7391067264a2f1
adamcurran123 <adamcurran129@gmail.com>
2024-12-04 15:36:43 +0000

    made create and read pages and implemented logic into NavBar for them

---
+++ datarep-proj/src/components/Create.js
@@ -0,0 +1,5 @@
+const Create = () => {
+    return <h3>Hello from the create component</h3>;
+  };
+  
+  export default Create;
---
+++ datarep-proj/src/components/Read.js
@@ -0,0 +1,5 @@
+const Read = () => {
+    return <h3>Hello from the read component</h3>;
+  };
+  
+  export default Read;
