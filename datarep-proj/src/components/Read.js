dc92e5744450a28ed3f7b2a72d56f5e0ca5849fa
adamcurran123 <adamcurran129@gmail.com>
2024-12-05 11:49:21 +0000

    filled up read.js with recipes using map function and also added css file to style that page

--- datarep-proj/src/components/Read.js
+++ datarep-proj/src/components/Read.js
@@ -1,5 +1,25 @@
+import './read.css';
+
 const Read = () => {
-    return <h3>Hello from the read component</h3>;
-  };
-  
-  export default Read;
+  const recipes = [
+      { id: 1, name: "Spaghetti Bolognese", description: "A classic Italian pasta dish with rich tomato sauce." },
+      { id: 2, name: "Chicken Curry", description: "A spicy and savory curry with tender chicken pieces." },
+      { id: 3, name: "Vegetable Stir-fry", description: "A quick and healthy stir-fry loaded with fresh veggies." },
+  ];
+
+  return (
+      <div>
+          <h3>Recipes</h3>
+          <ul>
+              {recipes.map((recipe) => (
+                  <li key={recipe.id}>
+                      <h4>{recipe.name}</h4>
+                      <p>{recipe.description}</p>
+                  </li>
+              ))}
+          </ul>
+      </div>
+  );
+};
+
+export default Read;
